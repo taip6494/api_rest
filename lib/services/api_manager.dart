5868e623991cc60b2689ebad1b45832c864f2435
taip6494 <palisseauthomas@gmail.com>
2022-01-24 22:09:14 +0400

    10.2 - Probléme résolu avec Uri.parse

--- lib/services/api_manager.dart
+++ lib/services/api_manager.dart
@@ -1,5 +1,5 @@
 import 'package:http/http.dart' as http;
-import 'package:api_rest/constants/strings.dart';
+//import 'package:api_rest/constants/strings.dart';
 
 class API_Manager {
   void getNews() async {
